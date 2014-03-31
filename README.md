d1a16982551f9a4fdb0922296c289299cd4a8b12
Kristian Glass <git@doismellburning.co.uk>
2014-03-31 16:13:42 +0100

    v0.0.0.0.0.etc

---
+++ README.md
@@ -0,0 +1,3 @@
+# myprs
+
+Trivial little tool - "show me open GitHub Pull Requests across organisation X"
---
+++ myprs.py
@@ -0,0 +1,29 @@
+import datetime
+import github3
+import os
+
+def myprs(organisation):
+    g = github3.login(token=os.environ['GITHUB_AUTH_TOKEN'])
+    org = g.organization(organisation)
+    prs = []
+    for repo in org.iter_repos():
+        for pr in repo.iter_pulls(state="open"):
+            prs += [pr]
+
+    yours = 0
+    for pr in prs:
+        r = pr.repository
+        line = "%s/%s: #%d: %s @ %s" % (r[0], r[1], pr.number, pr.title, pr.html_url)
+        if pr.user == g.user():
+            line += " (YOURS!)"
+            yours += 1
+        print line
+    print ""
+    print "%d open PRs" % len(prs)
+    print "%d are yours" % yours
+    print ""
+    print datetime.datetime.now().isoformat()
+
+if __name__ == "__main__":
+    import sys
+    myprs(sys.argv[1])
